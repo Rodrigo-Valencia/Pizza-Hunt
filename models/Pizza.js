63606bdb69c712baa916b4348ffaf2a4de6437cf
Rodrigo <rodvalenciaa@gmail.com>
2022-05-26 16:30:30 -0700

    integrate with the front end and refactor

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,6 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
 
 const PizzaSchema = new Schema ({
     pizzaName: {
@@ -9,7 +11,8 @@ const PizzaSchema = new Schema ({
     },
     createdAt: {
         type: Date, 
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
         type: String,
@@ -26,6 +29,7 @@ const PizzaSchema = new Schema ({
     {
         toJSON: {
             virtuals: true,
+            getters: true
         },
         id: false
     }
